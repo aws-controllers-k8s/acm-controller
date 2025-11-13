f831c2cc7ad044ff769fff12f6853fcf2a1ac8fa
Alex Wang <46534502+alexwang0311@users.noreply.github.com>
2025-11-12 16:20:38 -0800

    Fix update logic

--- pkg/resource/certificate/sdk.go
+++ pkg/resource/certificate/sdk.go
@@ -530,7 +530,6 @@ func (rm *resourceManager) sdkUpdate(
 		} else {
 			rlog.Info("Certificate export completed successfully")
 		}
-		return desired, nil
 	}
 
 	if delta.DifferentAt("Spec.Tags") {
--- templates/hooks/certificate/sdk_update_pre_build_request.go.tpl
+++ templates/hooks/certificate/sdk_update_pre_build_request.go.tpl
@@ -6,7 +6,6 @@
         } else {
             rlog.Info("Certificate export completed successfully")
         }
-		return desired, nil
     }
 
     if delta.DifferentAt("Spec.Tags") {
